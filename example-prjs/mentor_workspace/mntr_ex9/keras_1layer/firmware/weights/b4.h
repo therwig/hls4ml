2ad0ff6412fb19c7afeffbd7e95fb7c4298d4e96
Christian Herwig <therwig@fnal.gov>
2020-01-27 14:10:18 -0500

    first version of autoencoder

---
+++ example-prjs/mentor_workspace/mntr_ex9/keras_1layer/firmware/weights/b4.h
@@ -0,0 +1,15 @@
+//Numpy array shape [6]
+//Min -0.160342678428
+//Max 0.527373313904
+//Number of zeros 0
+
+#ifndef B4_H_
+#define B4_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b4[6];
+#else
+model_default_t b4[6] = {-0.1603, 0.5274, 0.1763, 0.2236, 0.4748, 0.0405};
+#endif
+
+#endif
---
+++ example-prjs/mentor_workspace/mntr_ex9/keras_1layer/firmware/weights/w6.h
@@ -0,0 +1,15 @@
+//Numpy array shape [6, 3]
+//Min -0.692249596119
+//Max 1.112214446068
+//Number of zeros 0
+
+#ifndef W6_H_
+#define W6_H_
+
+#ifndef __SYNTHESIS__
+model_default_t w6[18];
+#else
+model_default_t w6[18] = {-0.4117, 0.8673, -0.1577, 0.8218, 0.5910, 0.0049, -0.6194, 0.0724, 1.1122, 0.4105, 0.4421, 0.0391, 0.4234, -0.6922, 0.0813, -0.4470, -0.6619, 0.5951};
+#endif
+
+#endif
