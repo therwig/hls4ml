46c2169b697fc6bb8342661a0d4ca133387d99a4
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2020-01-29 21:55:35 -0500

    Update Verilog testbech

--- example-prjs/mentor_workspace/mntr_ex9/rtl-tb/tb/econV0_tb.v
+++ example-prjs/mentor_workspace/mntr_ex9/rtl-tb/tb/econV0_tb.v
@@ -1,14 +1,14 @@
-module keras1layer_tb;
+module econV0_tb;
 
 // Signals
 reg clk;
 reg reset;
 
-reg [179:0] input_data;
+reg [863:0] input_data;
 reg input_valid;
 wire input_ready;
 
-wire [17:0] output_data;
+wire [53:0] output_data;
 wire output_valid;
 reg output_ready;
 
@@ -18,21 +18,21 @@ wire size_in_valid;
 wire [15:0] size_out_data;
 wire size_out_ready;
 
-reg [17:0] expected_output_data;
+reg [53:0] expected_output_data;
 reg dut_error;
 
 // Module instance
-keras1layer keras1layer_top (
+econV0 econV0_top (
     .clk (clk),
     .rst (reset),
 
-    .input_1_rsc_dat (input_data), // IN
-    .input_1_rsc_vld (input_valid), // IN
-    .input_1_rsc_rdy (input_ready), // OUT
+    .input_48_rsc_dat (input_data), // IN
+    .input_48_rsc_vld (input_valid), // IN
+    .input_48_rsc_rdy (input_ready), // OUT
 
-    .layer5_out_rsc_dat (output_data), // OUT
-    .layer5_out_rsc_vld (output_valid), // OUT
-    .layer5_out_rsc_rdy (output_ready), // IN
+    .layer7_out_rsc_dat (output_data), // OUT
+    .layer7_out_rsc_vld (output_valid), // OUT
+    .layer7_out_rsc_rdy (output_ready), // IN
 
     .const_size_in_1_rsc_dat(size_in_data), // OUT
     .const_size_in_1_rsc_vld(size_in_valid), // OUT
@@ -44,7 +44,7 @@ keras1layer keras1layer_top (
 // Trace file setup.
 initial
 begin
-    $dumpfile ("keras1layer.vcd");
+    $dumpfile ("econV0.vcd");
     $dumpvars;
 end
 
@@ -62,7 +62,7 @@ initial
 begin
     $display ("@%04d INFO: ###################################################", $time);
 
-    input_data = 180'b0;
+    input_data = 864'b0;
     output_ready = 1'b0;
 
     // Assert input data as valid.
@@ -71,7 +71,7 @@ begin
     // Assert output data as ready.
     output_ready = 1'b1;
 
-    expected_output_data = 18'b0;
+    expected_output_data = 54'b0;
 
     dut_error = 0;
 end
@@ -92,25 +92,9 @@ initial begin
     // expected outputs.
     
     // 0
-    input_data = 180'b111111111110110110111111110000001111000000010001100110000000011001001010111111111111111100111111111111111100111111110011100101111111110010000101000000000111111011111111110110011001; 
-    expected_output_data = 18'b000000000000001111;
-
-    // 1 
-    //input_data = 180'b111111110010100000111111101100100110000000000111010011000000001010001000111111111110100111111111111110100111111111101110111100111111101101010100111111101111101011000000010011010100;
-    //expected_output_data = 18'b000000000000000100;
-
-    // 2
-    //input_data = 180'b111111111101010011000000000001011001111111110101101100111111110000110001111111111101110101111111111101110101111111111110101100111111111110100010111111110100000001000000001100111111;
-    //expected_output_data = 18'b000000000001110000;
-
-    // 3
-    //input_data = 180'b111111110101100101111111101100011110000000010101001001000000011001101111000000001001110001000000001001110001111111101111100111111111101101011010000000000101111010000000000000100011;
-    //expected_output_data = 18'b000000000000000110;
-
-    // 4
-    //input_data = 180'b111111111101010011000000000011001001111111110100011101111111110101100111111111111000110011111111111000110011111111111100110110111111111110110100000000000011000101111111111110101110;
-    //expected_output_data =   18'b000000000000001011;
-   
+    input_data = 863'b0; 
+    expected_output_data = 54'b000000000000000000000000010101111011000000111010001111;
+  
 end
 
 // Validation
